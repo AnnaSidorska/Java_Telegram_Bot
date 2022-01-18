e88361fb6e12039978de12c466e30a4008fe56e3
AnnaSidorska <annasidorska13@gmail.com>
2022-01-18 20:56:46 +0200

    Create StartBot.java

---
+++ src/main/java/StartBot.java
@@ -0,0 +1,16 @@
+import Core.BotCore;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class StartBot {
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new BotCore());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e.getCause());
+        }
+    }
+}
